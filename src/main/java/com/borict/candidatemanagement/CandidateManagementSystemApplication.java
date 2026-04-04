6efa23e77ebf083d0a2131feffc03bbc736610f0
Tamara Boric <tamaraboric@Tamaras-MacBook-Pro.local>
2026-04-04 13:15:51 +0200

    initial project setup

---
+++ src/main/java/com/borict/candidatemanagement/CandidateManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.borict.candidatemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/borict/candidatemanagement/CandidateManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borict.candidatemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
